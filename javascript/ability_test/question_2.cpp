288956144a72cc372c62f4629b7f304058a371d1
Yunika-Bajracharya <076bct095.yunika@pcampus.edu.np>
2022-01-04 16:32:13 +0545

    Add ability test

---
+++ javascript/ability_test/question_2.cpp
@@ -0,0 +1,36 @@
+// Find the Smallest and Biggest Numbers. Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+#include <iostream>
+using namespace std;
+
+void minMax(int array[], int size) {
+    int max = INT8_MIN;
+    int min = INT8_MAX;
+
+    for (int i = 0; i < size; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    
+    cout<<"["<<min<<", "<<max<<"]"<<endl;
+}
+
+int main() {
+    int size;
+    int *array = new int[size];
+
+    cout<<"Enter array's size: ";
+    cin>>size;
+    cout<<"Enter elements of array: ";
+    for (int i = 0; i < size; i++) {
+        cin>>array[i];
+    }
+
+    minMax(array, size);
+
+    return 0;
+}
