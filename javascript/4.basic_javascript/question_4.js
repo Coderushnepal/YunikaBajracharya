cafcf569f3b697ec45837ae6a9cc968d98da4be2
Yunika-Bajracharya <076bct095.yunika@pcampus.edu.np>
2022-01-11 08:52:00 +0545

    Add another solution for q4 in basic js

--- javascript/4.basic_javascript/question_4.js
+++ javascript/4.basic_javascript/question_4.js
@@ -1,10 +1,24 @@
 // Filter Repeating Character Strings
 
 function identicalFilter(arr) {
+
     let newArr = [];
+
     for (let i = 0; i < arr.length; i++) {
-        let set = new Set(arr[i]);
-        if (set.size == 1) {
+
+        var splittedItems = arr[i].split('');
+
+        var firstChar = splittedItems[0];
+        var isRepeating = true;
+
+        for (var j = 1; j < splittedItems.length; j++) {
+
+            if (splittedItems[j] != firstChar) {
+                isRepeating = false;
+            }
+        }
+
+        if (isRepeating) {
             newArr.push(arr[i]);
         }
     }
@@ -13,3 +27,16 @@ function identicalFilter(arr) {
 
 console.log( identicalFilter(["88", "999", "22", "545", "133"]) );
 console.log( identicalFilter(["xxxxo", "oxo", "xox", "ooxxoo", "oxo"]) );
+
+// Implementing using Set
+
+// function identicalFilter(arr) {
+//     let newArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         let set = new Set(arr[i]);
+//         if (set.size == 1) {
+//             newArr.push(arr[i]);
+//         }
+//     }
+//     return newArr;
+// }
